788a2a5cebce79916277ba46b57b0c378886c17c
Tom Fairclough <tom_fairclough@btinternet.com>
2023-01-23 11:36:58 +0000

    TF-Add Search and Submit Button

--- src/App.js
+++ src/App.js
@@ -2,10 +2,39 @@ import React, { Component } from 'react'
 import CountryList from './CountryList';
 
 class App extends Component{
+  constructor(props) {
+    super(props)
+    this.state ={
+      input: ''
+    }
+  }
+
+  inputHandler = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  handleSubmitClick = (e) => {
+    e.preventDefault()
+    console.log('Test')
+  }
+
+
+
+
   render() {
     return (
       <>
         <h1>Test</h1>
+        <form> 
+          <input 
+            type='text' 
+            placeholder='Country Name....'  
+            value={this.state.input} 
+            onChange={this.inputHandler}></input>
+          <button onClick={this.handleSubmitClick}>Submit</button>
+        </form>
         <CountryList/>
       </>
     )
