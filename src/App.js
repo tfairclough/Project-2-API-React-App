5b8468dc60491b952093b84511bf2ae12dd1c6c5
Tom Fairclough <tom_fairclough@btinternet.com>
2023-01-23 15:00:25 +0000

    TF- Filter countries on names+altspellings

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import React, { Component } from 'react'
 import CountryList from './CountryList';
 import FaveList from './FaveList';
-import axios from 'axios';
+import axios, { all } from 'axios';
 
 class App extends Component{
   constructor(props) {
     super(props)
     this.state ={
       input: '',
-      allCountries: '',
+      allCountries: [],
+      countriesToDisplay: [],
       faveList: ['A']
     }
   }
 
   componentDidMount = () => {
     const countryURL = 'https://restcountries.com/v3.1/all'
-    // axios.get(countryURL).then(response => (this.setState({allCountries: response.data})))
+    axios.get(countryURL).then(response => (this.setState({allCountries: response.data, countriesToDisplay: response.data})))
   }
 
   inputHandler = (e) => {
+    const textValue = e.target.value
+    const filteredCountries = this.state.allCountries.filter((country) => this.checkAllCountryNamesBool(country, textValue))
     this.setState({
-      input: e.target.value
+      input: textValue,
+      countriesToDisplay: filteredCountries
     })
   }
 
   handleSubmitClick = (e) => {
     e.preventDefault()
-    console.log('Test')
+    
   }
   
-
+  checkAllCountryNamesBool = (country, textValue) => {
+    const allNames = [country.name.common, country.name.official, ...country.altSpellings]
+    return allNames.some(name => name.toLowerCase().includes(textValue.toLowerCase())) ? true : false
+  }
 
 
 
@@ -45,9 +52,9 @@ class App extends Component{
             onChange={this.inputHandler}></input>
           <button onClick={this.handleSubmitClick}>Submit</button>
         </form>
-        {this.state.allCountries === '' ? <h1>Fetching.....</h1> 
+        {this.state.allCountries.length===0 ? <h1>Fetching.....</h1> 
           : <>
-            <CountryList allCountries={this.state.allCountries}/>
+            <CountryList allCountries={this.state.countriesToDisplay}/>
             <FaveList faveList={this.state.faveList}/>
           </>}
         
--- src/CountryDetails.js
+++ src/CountryDetails.js
@@ -3,7 +3,7 @@ import React from "react";
 const CountryDetails = (props) => {
   return (
     <>
-      <h5>Country: {props.country.name.common}</h5>
+      <h3>Country: {props.country.name.common}</h3>
       <p>Region: {props.country.region}</p>
       <img src={props.country.flags.png}/>
     </>
