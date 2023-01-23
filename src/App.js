701c7537d8009792a345e744c25c00fff3823293
Tom Fairclough <tom_fairclough@btinternet.com>
2023-01-23 10:58:16 +0000

    TF-Add CountryList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import CountryList from './CountryList';
 
 class App extends Component{
   render() {
     return (
       <>
         <h1>Test</h1>
+        <CountryList/>
       </>
     )
   }
